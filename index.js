922eb104841ac38dd634cd2c52603b87237c831a
Casey Capps <crcapps@gmail.com>
2019-10-23 17:59:48 -0400

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const db = require('./lib/db')
 const { Server } = require('./lib/http')
 
-db.init().then(blockChain => {
-  const server = Server(blockChain, db)
-  server.listen(process.env.PORT || 3000);
-}).catch(console.error)
+db.init()
+  .then(blockChain => {
+    const server = Server(blockChain, db)
+    server.listen(process.env.PORT || 3000)
+  })
+  .catch(console.error)
