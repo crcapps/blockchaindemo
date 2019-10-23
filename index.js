cafd1c2f3876c682207cbb6420485f210ce38392
Casey Capps <crcapps@gmail.com>
2019-10-23 12:54:33 -0400

    license, readme, lib

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const db = require('./lib/db')
+const { Server } = require('./lib/http')
+
+db.init().then(blockChain => {
+  const server = Server(blockChain, db)
+  server.listen(process.env.PORT || 3000);
+}).catch(console.error)
